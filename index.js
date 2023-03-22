8b83ee1141c0e3befc2b4908d8aa67e5a7d68251
waylon wickline <waylonw2077@LAPTOP-6MQVH55Q>
2023-03-22 15:09:30 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+  }
+function wrapAdjective() {
+    
+}
+function wrapAdjective(symbol = '*') {
+    return function(adjective) {
+      return `You are ${symbol}${adjective}${symbol}!`;
+    }
+  }
+const highlightWithStars = wrapAdjective('*');
+const highlightWithBars = wrapAdjective('||');
